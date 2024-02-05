0d65237f66da28a765fad295cf5b15b4c2dc1804
seungtoctoc <dltmxor31@naver.com>
2024-02-05 16:41:53 +0900

    feat: crawling title, link, detailUrl

--- crawlingWatchapedia.js
+++ crawlingWatchapedia.js
@@ -2,13 +2,36 @@ import axios from 'axios';
 import * as cheerio from 'cheerio'
 
 async function crawlingWatchapedia() {
-    const watchaUrl = 'https://pedia.watcha.com/ko-KR/';
-    const resp = await axios.get(watchaUrl);
-    const $watcha = cheerio
+    const watchaUrl = 'https://pedia.watcha.com/ko-KR/?domain=movie';
+    const watchaResp = await axios.get(watchaUrl);
+    const $watcha = cheerio.load(watchaResp.data);
+
+    const $movies = $watcha('li > a');
+
+    const result = [];
+
+    $movies
+        .map(async (idx, ele) => {
+            const title = $watcha(ele).prop('title');
+            
+            if (title) {
+                const link = $watcha(ele).prop('href');
+                const detailUrl = 'https://pedia.watcha.com/' + link;
+
+                
+
+                result.push({
+                    title: title,
+                    link: link,
+                    detailUrl: detailUrl
+                })
+            }
+
+            return null;
+        });
 
 
-
-
+    console.log(result);
 
 }
 
