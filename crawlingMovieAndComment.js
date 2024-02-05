f05f8ccd7f00201734e0b33010f4276b0ec6ef6f
seungtoctoc <dltmxor31@naver.com>
2024-02-06 08:58:56 +0900

    fix: modify file name, url

--- crawlingMovieAndComment.js
+++ crawlingMovieAndComment.js
@@ -2,33 +2,37 @@ import axios from 'axios';
 import * as cheerio from 'cheerio'
 
 async function crawlingWatchapedia() {
-    const watchaUrl = 'https://pedia.watcha.com/ko-KR/?domain=movie';
+    const movieSearchUrl = 'https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&qvt=0&query=%EB%B0%95%EC%8A%A4%EC%98%A4%ED%94%BC%EC%8A%A4';
     const watchaResp = await axios.get(watchaUrl);
     const $watcha = cheerio.load(watchaResp.data);
 
     const $movies = $watcha('li > a');
 
-    const result = [];
-
-    $movies
+    const result = await Promise.all($movies
         .map(async (idx, ele) => {
             const title = $watcha(ele).prop('title');
             
             if (title) {
                 const link = $watcha(ele).prop('href');
                 const detailUrl = 'https://pedia.watcha.com/' + link;
-
                 
+                const detailResp = await axios.get(detailUrl);
+                const $detail = cheerio.load(detailResp.data);
+                const score = $detail('div.css-1rqwswy.e15eo6m110').text().substring(0, 10);
 
-                result.push({
+
+
+                return ({
                     title: title,
                     link: link,
-                    detailUrl: detailUrl
+                    detailUrl: detailUrl,
+                    score: score
                 })
             }
 
             return null;
-        });
+        }).get()
+    );
 
 
     console.log(result);
