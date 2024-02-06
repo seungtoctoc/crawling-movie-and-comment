961e4b6f5d2884d8f207264f0796df1167e2ad38
seungtoctoc <dltmxor31@naver.com>
2024-02-06 09:09:59 +0900

    feat: fetch title, href link

--- crawlingMovieAndComment.js
+++ crawlingMovieAndComment.js
@@ -2,37 +2,25 @@ import axios from 'axios';
 import * as cheerio from 'cheerio'
 
 async function crawlingWatchapedia() {
-    const movieSearchUrl = 'https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&qvt=0&query=%EB%B0%95%EC%8A%A4%EC%98%A4%ED%94%BC%EC%8A%A4';
-    const watchaResp = await axios.get(watchaUrl);
-    const $watcha = cheerio.load(watchaResp.data);
+    const movieUrl = 'https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&qvt=0&query=%EB%B0%95%EC%8A%A4%EC%98%A4%ED%94%BC%EC%8A%A4';
+    const movieResp = await axios.get(movieUrl);
+    const $movie = cheerio.load(movieResp.data);
 
-    const $movies = $watcha('li > a');
+    const result = await $movie('div.cm_content_wrap li')
+        .map((idx, ele) => {
+            const title = $movie(ele).find('strong.name').text();
+            const href = $movie(ele).find('a').prop('href');
 
-    const result = await Promise.all($movies
-        .map(async (idx, ele) => {
-            const title = $watcha(ele).prop('title');
-            
-            if (title) {
-                const link = $watcha(ele).prop('href');
-                const detailUrl = 'https://pedia.watcha.com/' + link;
-                
-                const detailResp = await axios.get(detailUrl);
-                const $detail = cheerio.load(detailResp.data);
-                const score = $detail('div.css-1rqwswy.e15eo6m110').text().substring(0, 10);
+            return ({
+                title: title,
+                href: href
+            })
+        }).get();
 
+    console.log(result);
 
 
-                return ({
-                    title: title,
-                    link: link,
-                    detailUrl: detailUrl,
-                    score: score
-                })
-            }
 
-            return null;
-        }).get()
-    );
 
 
     console.log(result);
